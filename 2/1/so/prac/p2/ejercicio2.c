828aeebed3d739f91f964ab7d97a130bbaf495e8
ManuelBelgicano <manuelgbal1@gmail.com>
2018-10-29 10:31:25 +0100

    Avanzo con la sesion 2 de so

--- 2/1/so/prac/p2/ejercicio2.c
+++ 2/1/so/prac/p2/ejercicio2.c
@@ -1,21 +1,17 @@
-#include<sys/types.h>	
-#include<unistd.h>		
-#include<sys/stat.h>		
+#include<sys/types.h>
+#include<unistd.h>
+#include<sys/stat.h>
 #include<stdio.h>
 #include<errno.h>
 #include<stdlib.h>
 #include<dirent.h>
 #include<string.h>
 /*
-Realiza un programa en C utilizando las llamadas al sistema necesarias que acepte como entrada: 
+Realiza un programa en C utilizando las llamadas al sistema necesarias que acepte como entrada:
 • Un argumento que representa el 'pathname' de un directorio.
 • Otro argumento que es un número octal de 4 dígitos (similar al que se puede utilizar para cambiar los permisos en la llamada al sistema 
 chmod). Para convertir este argumento tipo cadena a un tipo numérico puedes utilizar la función strtol. Consulta el manual en línea para conocer sus argumentos.
 El programa tiene que usar el número octal indicado en el segundo argumento para cambiar los permisos  de  todos  los archivos  que se  encuentren  en el   directorio indicado  en  el  primer argumento.
-El programa debe proporcionar en la salida estándar una línea para cada archivo del directorio que esté formada por:
-<nombre_de_archivo> : <permisos_antiguos> <permisos_nuevos>
-Si no se pueden cambiar los permisos de un determinado archivo se debe especificar la siguiente información en la línea de salida:
-<nombre_de_archivo> : <errno> <permisos_antiguos>
 */
 void sintaxis() {
 	printf("USO DEL PROGRAMA:\nFormato de argumentos: ejercicio2 [pathname] [número_en_octal]\n");
