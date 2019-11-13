b8796d4c890aa090c6f73026a4d9bdd4ebe11807
Manuelbelgicano <manuelgbal1@gmail.com>
2019-11-13 18:16:29 +0100

    Hago prodcons LIFO con monitores SU

--- 3/1/scd/prac/p2/prodcons.cpp
+++ 3/1/scd/prac/p2/prodcons.cpp
@@ -1,6 +1,5 @@
 /**
- * @file prodcons.cpp
- * @author Manuel Gachs Ballegeer
+ * @file prod_cons_lifo.cpp
  */
 
 #include <iostream>
@@ -13,10 +12,14 @@
 using namespace std;
 using namespace HM;
 
+const int num_items = 40;   // número de items
+const int num_prod = 4; // Número de productores
+const int num_cons = 2; // Número de consumidores
+const int tam_vec = 10; // tamaño del buffer
+unsigned cont_prod[num_items] = {0}; // contadores de verificación: producidos
+unsigned cont_cons[num_items] = {0}; // contadores de verificación: consumidos
 mutex mtx;
-int tam_total; // Número total de items que se producen / leen
 
-//-------------------------------------------------------------------------------------------------
 // Función generadora de números aleatorios
 template<int min,int max> int aleatorio() {
 	static default_random_engine generador((random_device())());
@@ -24,119 +27,128 @@ template<int min,int max> int aleatorio() {
 	return distribucion_uniforme(generador);
 }
 
-//-------------------------------------------------------------------------------------------------
 // Función que produce un dato
 int producir_dato() {
 	static int contador = 0 ;
-	this_thread::sleep_for(std::chrono::milliseconds(aleatorio<20,100>()));
+	this_thread::sleep_for(chrono::milliseconds(aleatorio<20,100>()));
+	mtx.lock();
+	cout<<"producido: "<<contador<<endl<<flush;
+	mtx.unlock();
+	cont_prod[contador]++;
+	
 	return contador++;
 }
 
-//-------------------------------------------------------------------------------------------------
 // Función que consume un dato
-void consumir_dato() {
-	this_thread::sleep_for(std::chrono::milliseconds(aleatorio<20,100>()));
+void consumir_dato(unsigned dato) {
+	assert(dato<num_items);
+	cont_cons[dato]++;
+	this_thread::sleep_for(chrono::milliseconds(aleatorio<20,100>()));
+	mtx.lock();
+	cout<<"\tconsumido: "<<dato<<endl;
+	mtx.unlock();
 }
 
-//-------------------------------------------------------------------------------------------------
-// Monitor del productor-consumidor
+// Función que comprueba el correcto funcionamiento del programa
+void test_contadores() {
+	bool ok = true;
+	cout<<"Comprobando contadores ...\n";
+	for (size_t i=0;i<num_items;i++) {  
+		if (cont_prod[i]!=1) {
+			cout<<"ERROR: valor "<<i<<" producido "<<cont_prod[i]<<" veces."<<endl;
+			ok = false;
+		}
+		if (cont_cons[i]!=1) {
+			cout<<"ERROR: valor "<<i<<" consumido "<<cont_cons[i]<<" veces."<<endl;
+			ok = false;
+		}
+	}
+	if (ok)
+		cout<<endl<<flush<<"Solución (aparentemente) correcta."<<endl<<flush;
+}
+
+// Monitor
 class Monitor: public HoareMonitor {
 private:
-	int total; // Número de items a producir
-	int producidos; // Número de items producidos
-	int pila[20] = {0}; // Buffer
-	unsigned indice; // Indice de la pila
-	const static unsigned tam_vec = 20; // Tamaño de la pila
-	CondVar libres; // Semáforo productor
-	CondVar ocupadas; // Semáforo consumidor
+	const static unsigned tam_vec = 10; // Tamaño de la pila
+	int pila[tam_vec]; // Buffer
+	unsigned primera_libre; // Primera libre de la pila
+	CondVar libres; // Cola de espera de la hebra productora
+	CondVar ocupadas; // Cola de espera de la hebra consumidora
 public:
-	// Constructor
-	Monitor(int num);
-	// Función para consumir
-	void consumir();
-	// Función para producir
-	void producir();
+	// Constructor del monitor
+	Monitor();
+	// Función para leer un dato
+	int leer();
+	// Función para escribir un dato
+	void escribir(int dato);
 };
 
-Monitor::Monitor(int num) {
-	total = num;
-	producidos = 0;
-	indice = 0;
+Monitor::Monitor() {
+	primera_libre = 0;
 	libres = newCondVar();
 	ocupadas = newCondVar();
 }
 
-void Monitor::consumir() {
-	if (indice==0) // Si la pila está vacía espera
+int Monitor::leer() {
+	if (primera_libre<=0) // Espera si la pila está vacía
 		ocupadas.wait();
-	indice--;
-	int dato = pila[indice];
-	pila[indice] = 0;
-	mtx.lock();
-	cout<<"Se consume el dato: "<<dato<<endl;
-	mtx.unlock();
+	primera_libre--;
+	int dato =  pila[primera_libre];
+	libres.signal(); // Por si está esperando el productor
+	return dato;
 }
 
-void Monitor::producir() {
-	if ((indice+1)==tam_vec) // Si la pila está llena espera
+void Monitor::escribir(int dato) {
+	if (primera_libre==tam_vec) // Espera si la pila está llena
 		libres.wait();
-	int dato = producir_dato();
-	mtx.lock();
-	cout<<"Se produce el dato: "<<dato<<endl;
-	mtx.unlock();
-	pila[indice] = dato;
-	indice++;
-	ocupadas.signal();
+	pila[primera_libre] = dato;
+	primera_libre++;
+	ocupadas.signal(); // Por si está esperando el consumidor
 }
 
-//-------------------------------------------------------------------------------------------------
 // Función de la hebra productora
-void  funcion_hebra_productora(MRef<Monitor> monitor,unsigned k,unsigned j) {
-	for (size_t i=0;i<k;i++)
-		monitor->producir();
-	cout<<"\tTermina hebra productora número "<<j<<endl;
+void  funcion_hebra_productora(MRef<Monitor> monitor,int bloque,unsigned h) {
+	for (size_t i=0;i<bloque;i++) {
+		int dato = producir_dato();
+		monitor->escribir(dato);
+	}
+	mtx.lock();
+	cout<<"\t\tTermina hebra productora número: "<<h<<endl;
+	mtx.unlock();
 }
 
-//-------------------------------------------------------------------------------------------------
 // Función de la hebra consumidora
-void funcion_hebra_consumidora(MRef<Monitor> monitor,unsigned k,unsigned j) {
-	for (size_t i=0;i<k;i++)
-		monitor->consumir();
-	cout<<"\tTermina hebra consumidora número "<<j<<endl;
+void funcion_hebra_consumidora(MRef<Monitor> monitor,int bloque,unsigned h) {
+	for (size_t i=0;i<bloque;i++) {
+		int dato = monitor->leer();
+		consumir_dato(dato);
+	}
+	mtx.lock();
+	cout<<"\t\tTermina hebra consumidora número: "<<h<<endl;
+	mtx.unlock();
 }
 
-//-------------------------------------------------------------------------------------------------
 // Programa principal
 int main() {
-	cout<<"--------------------------------------------------------"<<std::endl;
-	cout<<"Problema de los productores-consumidores (solución LIFO)."<<std::endl;
-	cout<<"--------------------------------------------------------"<<std::endl<<std::flush;
+	cout<<"--------------------------------------------------------"<<endl;
+	cout<<"Problema de los productores-consumidores (solución LIFO)."<<endl;
+	cout<<"--------------------------------------------------------"<<endl<<flush;
 
-	unsigned num_prod;
-	unsigned num_cons;
-	cout<<"Introduce el número de items: ";
-	cin>>tam_total;
-	cout<<"Introduce el número de productores y de consumidores: ";
-	cin>>num_prod>>num_cons;
-	
-	unsigned cacho_p = tam_total/num_prod;
-	unsigned cacho_c = tam_total/num_prod;
-	thread productores[num_prod];
-	thread consumidores[num_cons];
-	MRef<Monitor> monitor = Create<Monitor>(tam_total);
+	MRef<Monitor> monitor = Create<Monitor>();
+	thread productoras[num_prod];
+	thread consumidoras[num_cons];
 
-	for (size_t i=0;i<num_prod;i++) {
-		if ((i+1)==num_prod)
-			cacho_p = tam_total - (i * cacho_p);
-		productores[i] = thread (funcion_hebra_productora,monitor,cacho_p,i);
-	}
-	for (size_t i=0;i<num_cons;i++) {
-		if ((i+1)==num_cons)
-			cacho_c = tam_total - (i * cacho_c);
-		productores[i] = thread (funcion_hebra_consumidora,monitor,cacho_c,i);
-	}
 	for (size_t i=0;i<num_prod;i++)
-		productores[i].join();
+		productoras[i] = thread (funcion_hebra_productora,monitor,num_items/num_prod,i);
 	for (size_t i=0;i<num_cons;i++)
-		consumidores[i].join();
+		consumidoras[i] = thread (funcion_hebra_consumidora,monitor,num_items/num_cons,i);
+
+	for (size_t i=0;i<num_prod;i++)
+		productoras[i].join();
+	for (size_t i=0;i<num_cons;i++)
+		consumidoras[i].join();
+	
+	cout<<"--------------------------------------------------------"<<endl;
+	test_contadores();
 }
